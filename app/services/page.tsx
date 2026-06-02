8ed541f2ee9a0c71eaa11780d3e856b0c579ac5e
ArsalanManak <arsalanayaz933@gmail.com>
2026-06-02 12:26:50 +0500

    Services page UI updated

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -122,7 +122,7 @@ export default function ServicesPage() {
         </div>
 
         {/* Bottom gradient fade */}
-        <div className="absolute bottom-0 left-0 right-0 h-24 bg-gradient-to-t from-[var(--navy-deep)] to-transparent pointer-events-none"></div>
+        <div className="absolute bottom-0 left-0 right-0 h-24 bg-gradient-to-t from-[var(--bg-main)] to-transparent pointer-events-none"></div>
       </section>
 
       {/* ═══ SERVICES LIST ═══ */}
